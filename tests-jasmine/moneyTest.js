a16784a852b5ff36e0a5ff274b8f4f23e7a74ebe
Bishwas Baral <bishwasbaral50@gmail.com>
2025-01-07 19:59:27 +0545

    chore: remove default tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { formattedPrice } from "../script/utils/pricing.js";
+
+describe('test suite: formatCurrency',() => {
+    it('convert cent into npr',() => {
+        expect(formattedPrice(2095)).toEqual('2,095');
+    });
+
+    it('works with 0',() => {
+        expect(formattedPrice(0)).toEqual('0');
+    });
+
+    it('works with decimal',() => {
+        expect(formattedPrice(2000.5)).toEqual('2,000.5');
+    });
+})
