d2f7feafbe361f9410a976f3161b37d365120aac
Bishwas Baral <bishwasbaral50@gmail.com>
2025-01-13 13:57:46 +0545

    chore: learn promises and convert code to promises

--- script/checkout.js
+++ script/checkout.js
@@ -1,15 +1,54 @@
 import { loadProducts } from "../data/product-lists.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
 
-new Promise((resolve) => {
-    console.log('promise');
-})
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve(); 
+        });
+    }),
+
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve(); 
+    });
+
+}).then(()=>{
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+//callbacks---------------->
+// loadProducts(()=>{
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
+
 
